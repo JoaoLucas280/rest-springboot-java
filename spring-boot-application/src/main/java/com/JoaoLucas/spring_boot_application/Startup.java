e38f2ff77d3a24155b93ec34e07d6a596c997985
JoaoLucas <joaolucasdev28@gmail.com>
2026-05-28 16:34:00 -0300

    First code with spring boot 3.4.0

---
+++ spring-boot-application/src/main/java/com/JoaoLucas/spring_boot_application/Startup.java
@@ -0,0 +1,13 @@
+package com.JoaoLucas.spring_boot_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ spring-boot-application/src/test/java/com/JoaoLucas/spring_boot_application/StartupTests.java
@@ -0,0 +1,13 @@
+package com.JoaoLucas.spring_boot_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
